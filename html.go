af827164b51c736e631a89aa3eab61595bddefdd
Rodrigo <rodrigo.desenv@gmail.com>
2018-08-18 23:22:41 -0300

    Multiplexador

--- html.go
+++ html.go
@@ -20,3 +20,17 @@ func Titulo(urls ...string) <-chan string {
 	}
 	return c
 }
+
+func encaminhar(origem <-chan string, destino chan string) {
+	for {
+		destino <- <-origem
+	}
+}
+
+//Juntar - misturar (mensagens) num canal
+func Juntar(entrada1, entrada2 <-chan string) <-chan string {
+	c := make(chan string)
+	go encaminhar(entrada1, c)
+	go encaminhar(entrada2, c)
+	return c
+}
